3ccaee1733f0d2d6f24c05716945bce9c1aea377
Thomas Carreira <thomas.crrrp@eduge.ch>
2016-06-02 08:19:21 +0200

    Ajout du répertoire Site et Documentation ainsi que le style du site.

---
+++ Site/index.php
@@ -0,0 +1,62 @@
+<?php
+session_start();
+require './librairie/librairie.php';
+require './librairie/librairie_concours.php';
+require './librairie/librairie_membres.php';
+
+
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Accueil</title>
+        <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+        <link href="bootstrap/css/tuto.css" rel="stylesheet">
+        <script src="https://code.jquery.com/jquery-2.2.3.min.js"   integrity="sha256-a23g1Nt4dtEYOj7bR+vTu7+T8VP13humZFBJNIYoEJo="   crossorigin="anonymous"></script>
+        <script src="./bootstrap/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <nav id="navigation" class="navbar navbar-default">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <div class="navbar-header">
+                        <a class="navbar-brand">Arc club Jussy</a>
+                    </div>
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul class="nav navbar-nav">
+                            <li class="active">
+                                <a href="index.php">Accueil</a>
+                            </li>
+                            <li>
+                                <a href="administration.php">Administration</a>
+                            </li>
+                            <li>
+                                <a href="connexion.php">Connexion <span class="glyphicon glyphicon-log-in" aria-hidden="true"></span></a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+            <div class="row">
+                <section class="col-sm-12 table-responsive">
+
+                </section>
+            </div>
+            <div class="row">
+                <footer class="col-sm-12">
+                    &copy Thomas Carreira
+                </footer>
+            </div>
+        </div>
+    </body>
+</html>
